78369575fd5c9c5e00c025e7981214adc328eb1b
Cédric Donner <cedonner@gmail.com>
2023-02-23 15:02:56 +0000

    implémentation des solutions

--- src/main.py
+++ src/main.py
@@ -1,2 +1,56 @@
 
 import utils
+
+def get_pixel(img: list[list[int]], x: int, y: int) -> int:
+    height = len(img)
+    width = len(img[0])
+
+    if x < 0 or x >= width:
+        return 0
+    elif y < 0 or y >= height:
+        return 0
+    else:
+        return img[y][x]
+
+def should_erode(img: list[list[int]], x: int, y: int) -> bool:
+    # Relative positions to watch for neighbor
+    left = (-1, 0)
+    right = (1, 0)
+    up = (0, -1)
+    down = (0, 1)
+    positions = [left, right, up, down]
+
+    count = 0
+    for dx, dy in positions:
+        if get_pixel(img, x + dx, y + dy):
+            count += 1
+    # print("count", count)
+
+    # pixel should be eroded if set to 1 and less than 4 neighbors are 1
+    return get_pixel(img, x, y) == 1 and count < 4
+
+
+def erosion(img: list[list[int]], n: int) -> list[list[int]]:
+    height = len(img)
+    width = height > 0 and len(img[0])
+    # Création d'une liste de la bonne dimension pour
+    # recevoir l'image destination
+    img2 = [[0] * width for _ in range(height)]
+
+
+    buffers = [img, img2]
+    src, dest = 0, 1
+    #print(buffers)
+    for _ in range(n):
+        for y, row in enumerate(buffers[src]):
+            for x, col in enumerate(row):
+                if should_erode(buffers[src], x, y):
+                    # print('eroding pixel', x, y)
+                    buffers[dest][y][x] = 0
+                else:
+                    buffers[dest][y][x] = buffers[src][y][x]
+        src, dest = dest, src
+
+    # comme on vient d'échanger src et dest, on retourne src
+    # pour retourner le dernier buffer dans lequel on a écrit
+    return buffers[src]
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,67 @@
+import os
 
-def img2ascii(img_data: list[list[int]], black: str, white: str) -> str:
+def img2ascii(
+    img_data: list[list[int]],
+    black: str = '#',
+    white: str = '.'
+) -> str:
+    return '\n'.join(
+        ''.join(black if pixel else white for pixel in row)
+        for row in img_data
+    )
 
-    return 'string bidon'
+def load_pbm(filename: str) -> list[list[int]]:
+    # image data
+    magic_code = ''
+    width = 0
+    height = 0
+
+    # grid of pixels, indexed by pixels[row][col]
+    pixels: list[list[int]] = [[]]
+    
+    print(f'loading {os.path.abspath(filename)}')
+
+    with open(filename, 'r') as imagefile:
+        lineno = 1
+
+        # pixel count
+        pixel_counter = 0
+        # row and column index
+        row, col = 0, 0
+
+        for line in imagefile:
+            line = line.strip()
+            if line.startswith('#'):
+                continue
+
+            if lineno == 1:
+                magic_code = line
+            elif lineno == 2:
+                width, height = [int(x) for x in line.split(' ')]
+                pixels = [[0] * width for _ in range(height)]
+                # print(width, height)
+            else:
+                # its a pixel data line
+                for pixel in line:
+                    if pixel == ' ':
+                        continue
+
+                    try:
+                        pixels[row][col] = int(pixel)
+                    except Exception as e:
+                        print(f"Error : {e}. row={row}, col={col}, pixel={pixel}")
+                        break
+
+                    pixel_counter += 1
+                    col += 1
+
+                    if col >= width:
+                        col = 0
+                        row += 1
+
+            lineno += 1
+    return pixels
+
 
 if __name__ == '__main__':
     import doctest
