41739e2242b25cae4139ad2aad156f4b73cbee6f
Cédric Donner <cedonner@gmail.com>
2023-02-24 08:51:41 +0000

    Mise en place des fichiers de départ

--- src/main.py
+++ src/main.py
@@ -1,2 +1,6 @@
 
-import utils
+from utils import load_pbm, img2ascii
+
+
+def erosion(img: list[list[int]], n: int) -> list[list[int]]:
+    ...
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,15 @@
+import os
 
-def img2ascii(img_data: list[list[int]], black: str, white: str) -> str:
+def img2ascii(
+    img_data: list[list[int]],
+    black: str = '#',
+    white: str = '.'
+) -> str:
+    ...
 
-    return 'string bidon'
+def load_pbm(filename: str) -> list[list[int]]:
+    ...
+
 
 if __name__ == '__main__':
     import doctest
